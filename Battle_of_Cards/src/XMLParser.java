966b0c1cce19697d9284880a6524a7761162fa8c
Your Name <you@example.com>
2020-05-05 01:33:42 +0200

    create loadXMLDocument method in XMLParser class

--- Battle_of_Cards/src/XMLParser.java
+++ Battle_of_Cards/src/XMLParser.java
@@ -1,1 +1,16 @@
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
+public class XMLParser {
+
+    public Document loadXMLDocument(String xmlPath) throws SAXException, IOException, ParserConfigurationException{
+
+        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlPath);
+        document.getDocumentElement().normalize();
+        return document;
+    }
+}
