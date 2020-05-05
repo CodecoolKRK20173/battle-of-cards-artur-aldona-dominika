3dd9525839408f73cb9312d9df6a730c2c97e84b
Your Name <you@example.com>
2020-05-05 21:06:08 +0200

    create constructor in Table class

--- Battle_of_Cards/src/Table.java
+++ Battle_of_Cards/src/Table.java
@@ -5,4 +5,8 @@ public class Table {
     private XMLParser xml = new XMLParser();
     private List<Card> cards;
 
+    public Table() {
+        this.cards = xml.CardParser(xml.loadXMLDocument("src/resources/DragonLeague.xml"));
+    }
+
 }
