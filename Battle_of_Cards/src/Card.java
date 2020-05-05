baa60f60f9fca6ef293729656cdb9a4c021f9f61
Your Name <you@example.com>
2020-05-05 19:44:37 +0200

    add getters in  Card class

--- Battle_of_Cards/src/Card.java
+++ Battle_of_Cards/src/Card.java
@@ -6,4 +6,36 @@ public class Card {
     private int endurance;
     private int agility;
 
+    public Card(String name, int intellingence, int strength, int endurance,  int agility) {
+        this.name = name;
+        this.intelligence = intellingence;
+        this.strength = strength;
+        this.endurance = endurance;
+        this.agility = agility;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public int getIntelligence() {
+
+        return intelligence;
+    }
+
+    public int getStrength() {
+
+        return strength;
+    }
+
+    public int getEndurance() {
+
+        return endurance;
+    }
+
+   public int getAgility() {
+
+        return agility;
+    }
 }
