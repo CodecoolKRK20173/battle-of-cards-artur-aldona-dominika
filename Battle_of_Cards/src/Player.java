ff7d049e2dca59bd56a2ff83211ba9428e16461f
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-05-06 15:02:42 +0200

    added chooseAtribiuteToPlay and takeCardsFromTable(empty method for now)

--- Battle_of_Cards/src/Player.java
+++ Battle_of_Cards/src/Player.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public abstract class Player {
 
     protected String name;
@@ -6,9 +8,16 @@ public abstract class Player {
         this.name = name;
     }
 
-    public String getName(){
+    protected String getName(){
         return name;
     }
 
+    protected abstract int chooseAtribiuteToPlay();
+    protected abstract List<Card> takeCardsFromTable();
 
+    // hand - lista kart ktore Player otrzymuje od table (16) pozniej bedzie wiecej, private
+    // jesli do hand trafia dwie karty (wygrana) to jest ruch gracza (jakis boolean = True)
+    // gracz ktory przegra nie dostaje dwoch kart (przegrana) to jest ruch przeciwnika (jakis boolean = Fase)
+    // ....gracz decyduje jakim atrybutem bedzie rozgrywka np strengh (wywolanie metody jest w engine)
+    // jesli przeciwnik wybral atrybut to gracz musi grac tym atrybutem
 }
--- Battle_of_Cards/src/UserPlayer.java
+++ Battle_of_Cards/src/UserPlayer.java
@@ -1,8 +1,39 @@
+import java.util.List;
+
 public class UserPlayer extends Player {
 
     public UserPlayer(String name) {
         super(name);
     }
 
+    Card card;
+    Table table;
+    int userChoosedAtribiute; // Input class will input user choice
+    List<Card> playerCardsSet;
 
+    @Override
+    protected int chooseAtribiuteToPlay() {
+        int userChoice = 1;
+
+        switch (userChoosedAtribiute){
+            case 1:
+                userChoice = card.getIntelligence();
+                break;
+            case 2:
+                userChoice = card.getStrength();
+                break;
+            case 3:
+                userChoice = card.getEndurance();
+                break;
+            case 4:
+                userChoice = card.getAgility();
+                break;
+        }
+        return userChoice;
+    }
+
+    @Override
+    protected List<Card> takeCardsFromTable() {
+        return null;
+    }
 }
