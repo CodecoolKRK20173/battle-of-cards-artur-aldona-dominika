67d3f3b9d4f857a58f02ccde55466c372ab1ece0
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-05-06 17:20:55 +0200

    changed xml path at Table class in order to resolve any conflicts with merging (again)

--- Battle_of_Cards/src/Player.java
+++ Battle_of_Cards/src/Player.java
@@ -18,6 +18,6 @@ public abstract class Player {
     // playerCardsSet (hand) - lista kart ktore Player otrzymuje od table (16) pozniej bedzie wiecej, private
     // jesli do hand trafia dwie karty (wygrana) to jest ruch gracza (jakis boolean = True)
     // gracz ktory przegra nie dostaje dwoch kart (przegrana) to jest ruch przeciwnika (jakis boolean = Fase)
-    // ....gracz decyduje jakim atrybutem bedzie rozgrywka np strengh (wywolanie metody jest w engine)
+    // ...gracz decyduje jakim atrybutem bedzie rozgrywka np strengh (wywolanie metody jest w engine)
     // jesli przeciwnik wybral atrybut to gracz musi grac tym atrybutem
 }
