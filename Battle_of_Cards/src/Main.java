c68d439d7e4837d15b9ab678636c39331905ece6
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-05-06 17:12:06 +0200

    added two more methods, still work in progress

--- Battle_of_Cards/src/Main.java
+++ Battle_of_Cards/src/Main.java
@@ -1,14 +1,9 @@
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-
 public class Main {
 
 
-    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
-//        Engine gameStart = new Engine();
-//        gameStart.start();
+    public static void main(String[] args) {
+        Engine gameStart = new Engine();
+        gameStart.start();
 
     }
 }
