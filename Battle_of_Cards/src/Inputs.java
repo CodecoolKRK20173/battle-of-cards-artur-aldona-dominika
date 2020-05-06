75464fd86bcfbc1481e0711c6e766a8382b5cd1a
Your Name <you@example.com>
2020-05-06 14:52:10 +0200

    refactor getUserInput method

--- Battle_of_Cards/src/Inputs.java
+++ Battle_of_Cards/src/Inputs.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 class Inputs {
+    private Scanner scanner = new Scanner(System.in);
 
-    public void takeInput(String messageForUser) {
-        Scanner input = new Scanner(System.in);
-        System.out.println(messageForUser);
-        input.nextLine();
+    public String getUserInput() {
+        String input = scanner.nextLine();
+        System.out.println();
+        return input;
     }
 }
 
